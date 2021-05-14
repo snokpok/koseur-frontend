d61ff0fedd3acd2cdd210a4cff3387e9fbea7745
snokpok <vinceny.fe@gmail.com>
2021-05-15 00:25:30 +0700

    got it

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar/Navbar'
 import styles from './Header.module.sass'
+import Image from 'next/image'
 
 export interface HeaderProps {
     barName: string
 }
 
 export default function Header(props: HeaderProps) {
+
     return (
         <div className={styles.Header}>
             <div className={styles.BarName}>
--- pages/speakeasy/index.tsx
+++ pages/speakeasy/index.tsx
@@ -1,25 +1,38 @@
 import React, { Component } from 'react'
 import Header from '../../components/Header/Header'
 import Image from 'next/image'
+import styles from '../../styles/styles.module.sass'
 
-export default class BarPage extends Component {
+export default function BarPage() {
+    const sideLength = 130
 
-    render() {
-        return (
-            <div style={{
-                height: "100vh"
-            }}>
-                <Header
-                    barName="SpeakEasy"
+    return (
+        <div>
+            <div className={styles.bgWrap}>
+                <Image
+                    src='/bar-background2.jpg'
+                    layout="fill"
+                    objectFit="cover"
+                    quality={100}
                 />
-                <div>
-                    <Image
-                        src="/logo.png"
-                        width={50}
-                        height={50}
-                    />
-                </div>
+            </div >
+            <Header
+                barName="SpeakEasy"
+            />
+            <div
+                className={styles.Logo}
+            >
+                <Image
+                    src="/logo.jpg"
+                    width={sideLength}
+                    height={sideLength}
+                />
             </div>
-        )
-    }
+            <p className={styles.bgText}>
+                Bring me closer...
+                    <br />
+                    baby
+                </p>
+        </div>
+    )
 }
