69135366d8e2b9b3b7e8335c8c710f83f3cffd34
snokpok <vinceny.fe@gmail.com>
2021-05-17 22:35:19 +0700

    never push straight to master

--- components/CategorySubsection/CategorySubsection.tsx
+++ components/CategorySubsection/CategorySubsection.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import styles from "./CategorySubsection.module.sass";
 import CategoryItem, { ICategoryItem } from "../CategoryItem/CategoryItem";
+import ScrollContainer from 'react-indiana-drag-scroll';
 
+// ICategorySubsection
 export interface ICategorySubsection {
     name: string;
     bars: ICategoryItem[];
 }
 
 export default function CategorySubsection(props: ICategorySubsection) {
+    
     return (
         <div className={styles.CategorySubsection}>
             <div className={styles.Header}>{props.name}</div>
+            <ScrollContainer className={styles.Items} horizontal = {true} vertical={false} hideScrollbars={true}>
+                {props.bars.map((item: ICategoryItem) => (
+                            <CategoryItem {...item} />
+                        ))}
+            </ScrollContainer>
+            {/* old scroll
             <div className={styles.Items}>
                     {props.bars.map((item: ICategoryItem) => (
                         <CategoryItem {...item} />
                     ))}
-            </div>
+            </div> */}
         </div>
     );
 }
