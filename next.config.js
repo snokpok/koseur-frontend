3062acb53897301c42e2d975682ba493d93fa4c2
snokpok <vinceny.fe@gmail.com>
2021-05-17 13:08:49 +0700

    removed zeit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const {PHASE_DEVELOPMENT_SERVER} = require("next/constants");
-const withCSS = require("@zeit/next-css");
-module.exports = withCSS({});
 module.exports = (phase, {defaultConfig}) => {
     return {
         env: {
