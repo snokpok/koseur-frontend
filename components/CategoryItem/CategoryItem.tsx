c99557c0d144a3b50093aa530e482d95a449dae1
Trung Vu <trung.vt@geekup.vn>
2021-05-19 16:07:30 +0700

    trying to do styles in home page

--- components/CategoryItem/CategoryItem.tsx
+++ components/CategoryItem/CategoryItem.tsx
@@ -3,36 +3,46 @@ import styles from "./CategoryItem.module.sass";
 import Image from "next/image";
 import { ImageLoaderFunction } from "../../commons/utils/image-loader.function";
 import { Bar } from "../../commons/graphql/schema-interfaces";
+import FadeInImage from "../FadeInImage/FadeInImage";
 
-export default function CategoryItem(props: { bar: Bar }) {
+export default function CategoryItem({bar}: { bar: Bar }) {
     const ITEM_SIZE_RATIO = 7 / 8;
     const [rightWidth, setRightWidth] = useState<number>(0);
     const [rightHeight, setRightHeight] = useState<number>(0);
 
     useEffect(() => {
-        if (props.bar.logo) {
+        if (bar.logo) {
             setRightWidth(
-                props.bar.logo.formats.thumbnail.width * ITEM_SIZE_RATIO
+                bar.logo.formats.thumbnail.width * ITEM_SIZE_RATIO
             );
             setRightHeight(
-                props.bar.logo.formats.thumbnail.height * ITEM_SIZE_RATIO
+                bar.logo.formats.thumbnail.height * ITEM_SIZE_RATIO
             );
         }
     });
 
     return (
         <div className={styles.CategoryItem}>
-            <div>{props.bar.name}</div>
+            {/* <FadeInImage
+                loader={ImageLoaderFunction}
+                src={bar.logo?.formats.thumbnail.url ?? null}
+                layout="fill"
+                objectFit="cover"
+                quality={100}
+            /> */}
+            <div>{bar.name}</div>
             <a href="http://localhost:8080">a link</a>
-            <div>{props.bar.description}</div>
-            {props.bar.logo ? (
-                <Image
-                    loader={ImageLoaderFunction}
-                    src={props.bar.logo.formats.thumbnail.url}
-                    width={rightWidth}
-                    height={rightHeight}
-                />
-            ) : null}
+            <div>{bar.description}</div>
+            <div>
+                {bar.logo ? (
+                    <Image
+                        loader={ImageLoaderFunction}
+                        src={bar.logo.formats.thumbnail.url}
+                        width={rightWidth}
+                        height={rightHeight}
+                    />
+                ) : null}
+            </div>
         </div>
     );
 }
