ef5243abd50548db439e4ba04438db3310fb2944
snokpok <vinceny.fe@gmail.com>
2021-05-20 09:28:13 +0700

    why is fallback on non-avail locales not working

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect , useState } from "react";
 import styles from "../../styles/styles.module.sass";
 import Head from "next/head";
 import FadeInImage from "../../components/FadeInImage/FadeInImage";
 import { homePageData } from "../../commons/mock-data/home-page";
+import { homePageHanoiData } from "../../commons/mock-data/home-page-hanoi";
+import { homePageHCMData } from "../../commons/mock-data/home-page-hcm";
 import CategorySection from "../../components/CategorySection/CategorySection";
 import { NextPageContext } from "next";
 import { getBarsHomePageQueryVariables } from "../../commons/graphql/qvs";
@@ -12,11 +14,24 @@ import { Category } from "../../commons/graphql/schema-interfaces";
 export interface HomePageProps {
     data: {
         categories: Category[];
-    };
+    },
+    city?: string
 }
 
-export default function HomePage({ data }: HomePageProps) {
+export default function HomePage({ data, city }: HomePageProps) {
     const sideLength = 100;
+    const [dataHome, setDataHome] = useState({data: {categories: {}}})
+
+    useEffect(() => {
+        switch (city) {
+            case "Hanoi":
+                setDataHome(homePageHanoiData)
+            case "HCM":
+                setDataHome(homePageHCMData)
+            default: 
+                setDataHome(homePageData)
+        }
+    }, [])
 
     return (
         <>
@@ -33,7 +48,8 @@ export default function HomePage({ data }: HomePageProps) {
                 <div className={styles.IntroHomePage}>Styles</div>
                 <CategorySection
                     categories={
-                        data?.categories ?? homePageData.data.categories
+                        data?.categories 
+                            ?? dataHome.data.categories
                     }
                 />
             </div>
@@ -44,10 +60,10 @@ export default function HomePage({ data }: HomePageProps) {
 HomePage.getInitialProps = async (ctx: NextPageContext) => {
     try {
         const res = await AxiosGenericQueryFunction(
-            getBarsHomePageQueryVariables("id")
+            getBarsHomePageQueryVariables("id", {city: ctx.query.city})
         );
         return res.data;
     } catch {
-        return { data: null };
+        return { data: null, city: ctx.query.city };
     }
 };
