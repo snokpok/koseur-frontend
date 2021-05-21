bdc4de192257c4461f745e86ad96f5b129495a25
snokpok <vinceny.fe@gmail.com>
2021-05-21 20:03:49 +0700

    no drag at <2 catitems + modularize carousel code

--- components/CategorySubsection/CategorySubsection.tsx
+++ components/CategorySubsection/CategorySubsection.tsx
@@ -1,17 +1,25 @@
-import React, { useCallback, useState } from "react";
+import React, { ReactElement, useCallback, useState } from "react";
 import styles from "./CategorySubsection.module.sass";
 import CategoryItem from "../CategoryItem/CategoryItem";
 import { Bar, Category, Maybe } from "../../commons/graphql/schema-interfaces";
 import Carousel from "nuka-carousel";
-import {IoIosArrowDropright, IoIosArrowDropleft} from 'react-icons/io'
+import { IoIosArrowDropright, IoIosArrowDropleft } from "react-icons/io";
+import { IconType } from "react-icons/lib";
 
 export default function CategorySubsection(props: { category: Category }) {
-    const buttonStyle = {
-        borderRadius: "50%",
-        width: "4rem",
-        height: "4rem",
-        opacity: 0.7,
-        backgroundColor: "black",
+    const handleCenterControl = (
+        slideCallback: () => void,
+        controlElement: JSX.Element
+    ) => {
+        if (props.category!.bars!.length > 2)
+            return (
+                <div
+                    style={{ fontSize: 30, cursor: "pointer" }}
+                    onClick={slideCallback}
+                >
+                    {controlElement}
+                </div>
+            );
     };
 
     return (
@@ -22,27 +30,20 @@ export default function CategorySubsection(props: { category: Category }) {
                 disableEdgeSwiping={true}
                 slidesToShow={2}
                 autoplay={true}
+                dragging={props.category!.bars!.length > 2}
                 cellSpacing={50}
                 renderBottomCenterControls={null}
-                defaultControlsConfig={{
-                    nextButtonText: ">",
-                    prevButtonText: "<",
-                    nextButtonStyle: buttonStyle,
-                    prevButtonStyle: buttonStyle,
-                }}
-                renderCenterRightControls={
-                    ({nextSlide}) => {
-                        return <div style={{fontSize: 30, cursor: "pointer"}} onClick={nextSlide}>
-                            <IoIosArrowDropright className={styles.ArrowIcon}/>
-                        </div>
-                    }
+                renderCenterRightControls={({ nextSlide }) =>
+                    handleCenterControl(
+                        nextSlide,
+                        <IoIosArrowDropright className={styles.ArrowIcon} />
+                    )
                 }
-                renderCenterLeftControls={
-                    ({previousSlide}) => {
-                        return <div style={{fontSize: 30, cursor: "pointer"}} onClick={previousSlide}>
-                            <IoIosArrowDropleft className={styles.ArrowIcon}/>
-                        </div>
-                    }
+                renderCenterLeftControls={({ previousSlide }) =>
+                    handleCenterControl(
+                        previousSlide,
+                        <IoIosArrowDropleft className={styles.ArrowIcon} />
+                    )
                 }
                 style={{
                     overflow: "auto",
