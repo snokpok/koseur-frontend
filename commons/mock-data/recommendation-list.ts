747f478a48b9e58aa7d28e3e3d09a39f3fbdce07
snokpok <vinceny.fe@gmail.com>
2021-06-01 10:22:30 +0700

    recommendation mock data

---
+++ commons/mock-data/recommendation-list.ts
@@ -0,0 +1,73 @@
+export const recommendationList = {
+    data: {
+        drinks: [
+            {
+                id: "2",
+                name: "BOOZE and HOOCH",
+                description: null,
+                images: [
+                    {
+                        width: 5010,
+                        height: 3340,
+                        name: "drink_2.jpg",
+                        url: "/uploads/drink_2_0f51bcfbbc.jpg",
+                    },
+                ],
+            },
+            {
+                id: "3",
+                name: "Fall in Love with The Moon",
+                description: null,
+                images: [
+                    {
+                        width: 5051,
+                        height: 3367,
+                        name: "drink_3.jpg",
+                        url: "/uploads/drink_3_62bfaf3ed9.jpg",
+                    },
+                ],
+            },
+            {
+                id: "4",
+                name: "HEAVEN in HELL",
+                description: null,
+                images: [
+                    {
+                        width: 5760,
+                        height: 3840,
+                        name: "drink_4.jpg",
+                        url: "/uploads/drink_4_2c96e91d19.jpg",
+                    },
+                ],
+            },
+            {
+                id: "5",
+                name: "Apple Martini",
+                description:
+                    "The principal ingredients of the appletini, as it came to be known, are vodka and Sour Apple Pucker, a radiant, possibly radioactive green schnapps that gives the drink its signature hue and melted Jolly Rancher taste.",
+                images: [
+                    {
+                        width: 2121,
+                        height: 1414,
+                        name: "apple-martini.png",
+                        url: "/uploads/apple_martini_c4f54c24cb.png",
+                    },
+                ],
+            },
+            {
+                id: "7",
+                name: "Sazerac",
+                description:
+                    "If you like a drink with some bite, give this classic New Orleans concoction a try.",
+                images: [
+                    {
+                        width: 980,
+                        height: 1470,
+                        name: "sazerac.png",
+                        url: "/uploads/sazerac_29d0801213.png",
+                    },
+                ],
+            },
+        ],
+    },
+};
